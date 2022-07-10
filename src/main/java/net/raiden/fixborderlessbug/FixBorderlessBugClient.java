a422969436fee70bd487c8c7ee326632d3c6c6fe
Raidenxd2 <64755433+Raidenxd2@users.noreply.github.com>
2022-07-10 14:20:08 -0400

    Initial commit

---
+++ src/main/java/net/raiden/fixborderlessbug/FixBorderlessBugClient.java
@@ -0,0 +1,11 @@
+package net.raiden.fixborderlessbug;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FixBorderlessBugClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
